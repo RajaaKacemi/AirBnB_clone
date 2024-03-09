8ecd958458f43a7781ef2a25063eabc99e102609
RajaaKacemi <98607846+RajaaKacemi@users.noreply.github.com>
2024-03-09 17:34:06 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,43 +15,35 @@ from models.city import City
 
 class FileStorage:
     """
-    FileStorage class for storing, serializing and deserializing data
+    FileStorage class for storing, serializing, and deserializing data
     """
     __file_path = "file.json"
-
     __objects = {}
 
     def new(self, obj):
         """
-         Sets an object in the __objects dictionary with a key of 
-         <obj class name>.id.
+        Sets an object in the __objects dictionary with a key of
+        <obj class name>.id.
         """
         obj_cls_name = obj.__class__.__name__
-
         key = "{}.{}".format(obj_cls_name, obj.id)
-
         FileStorage.__objects[key] = obj
-
 
     def all(self):
         """
-        Returns the __objects dictionary. 
+        Returns the __objects dictionary.
         It provides access to all the stored objects.
         """
-        return  FileStorage.__objects
-
+        return FileStorage.__objects
 
     def save(self):
         """
-        Serializes the __objects dictionary into 
+        Serializes the __objects dictionary into
         JSON format and saves it to the file specified by __file_path.
         """
-        all_objs = FileStorage.__objects
-
         obj_dict = {}
-
-        for obj in all_objs.keys():
-            obj_dict[obj] = all_objs[obj].to_dict()
+        for key, obj in FileStorage.__objects.items():
+            obj_dict[key] = obj.to_dict()
 
         with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
             json.dump(obj_dict, file)
@@ -67,10 +59,25 @@ class FileStorage:
 
                     for key, value in obj_dict.items():
                         class_name, obj_id = key.split('.')
-
                         cls = eval(class_name)
 
-                        instance = cls(**value)
+                        # Dynamically create instance based on class name
+                        if class_name == 'BaseModel':
+                            instance = BaseModel(**value)
+                        elif class_name == 'User':
+                            instance = User(**value)
+                        elif class_name == 'Amenity':
+                            instance = Amenity(**value)
+                        elif class_name == 'Place':
+                            instance = Place(**value)
+                        elif class_name == 'Review':
+                            instance = Review(**value)
+                        elif class_name == 'State':
+                            instance = State(**value)
+                        elif class_name == 'City':
+                            instance = City(**value)
+                        else:
+                            continue
 
                         FileStorage.__objects[key] = instance
                 except Exception:
